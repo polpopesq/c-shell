defdb7e61887d1c1b628f87d8ad3a62aacf482e1
Paul@WSL <paul.popescu2003@gmail.com>
2026-01-11 11:30:46 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+
+  char buffer[1024];
+  fgets(buffer, sizeof(buffer), stdin);
+  buffer[strcspn(buffer, "\n")] = '\0';
+
+  printf("%s: command not found\n", buffer);
 
   return 0;
 }
