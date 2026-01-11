23935f5ce056759b30462effdc756b69bc1d8096
Paul@WSL <paul.popescu2003@gmail.com>
2026-01-11 17:00:09 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  printf("$ ");
-
   char buffer[1024];
-  fgets(buffer, sizeof(buffer), stdin);
-  buffer[strcspn(buffer, "\n")] = '\0';
 
-  printf("%s: command not found\n", buffer);
+  while (1)
+  {
+    printf("$ ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    printf("%s: command not found\n", buffer);
+  }
 
   return 0;
 }
