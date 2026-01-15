b8cb5fe42d7b78320a9388bc5cf3bc3c391c9b7b
Paul@WSL <paul.popescu2003@gmail.com>
2026-01-15 16:24:20 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <string.h>
 #include <dirent.h>
-#include <unistd.h>
 #include <sys/stat.h>
-#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define _POSIX_C_SOURCE 200809L // to show strtok_r definition
 #ifdef _WIN32
@@ -35,7 +37,8 @@ enum Command get_command_type(const char *s)
 typedef struct
 {
   char *cmd;
-  char *args;
+  char *argv[16]; // tokenized command + args
+  int argc;
   enum Command type;
 } ParsedCommand;
 
@@ -44,21 +47,24 @@ ParsedCommand parse_command(char *line)
   line[strcspn(line, "\n")] = '\0';
 
   ParsedCommand out = {0};
-  char *space = strchr(line, ' ');
 
-  if (space)
+  int i = 0;
+  char *token = strtok(line, " ");
+
+  while (token && i < 15)
   {
-    *space = '\0';
-    out.cmd = line;
-    out.args = space + 1;
+    out.argv[i++] = token;
+    token = strtok(NULL, " ");
   }
-  else
+  out.argv[i] = NULL;
+  out.argc = i;
+
+  if (i > 0)
   {
-    out.cmd = line;
-    out.args = NULL;
+    out.cmd = out.argv[0];
+    out.type = get_command_type(out.cmd);
   }
 
-  out.type = get_command_type(out.cmd);
   return out;
 }
 
@@ -124,14 +130,13 @@ char *path_lookup(const char *PATH, const char *command)
   return NULL;
 }
 
-void type(const char *s)
+void type(const char *s, const char *PATH)
 {
   if (!s)
     return;
 
   if (get_command_type(s) == UNKNOWN)
   {
-    const char *PATH = getenv("PATH");
     if (!PATH)
     {
       printf("%s: not found\n", s);
@@ -155,11 +160,45 @@ void type(const char *s)
   }
 }
 
+static int exec_prog(char *const argv[])
+{
+  pid_t pid = fork();
+
+  if (pid == -1)
+  {
+    perror("fork failed");
+    return -1;
+  }
+
+  if (pid == 0)
+  {
+    execvp(argv[0], argv);
+    perror("execvp failed");
+    _exit(127);
+  }
+
+  // parent: wait for child to finish
+  int status;
+  if (waitpid(pid, &status, 0) == -1)
+  {
+    perror("waitpid failed");
+    return -1;
+  }
+
+  if (WIFEXITED(status))
+  {
+    return WEXITSTATUS(status);
+  }
+
+  return -1;
+}
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
   char buffer[1024];
+  const char *PATH = getenv("PATH");
 
   while (1)
   {
@@ -173,13 +212,28 @@ int main(int argc, char *argv[])
     case EXIT:
       return 0;
     case ECHO:
-      printf("%s\n", command.args);
+      for (int i = 1; i < command.argc; i++)
+      {
+        printf("%s ", command.argv[i]);
+      }
+      printf("\n");
       break;
     case TYPE:
-      type(command.args);
+      type(command.argv[1], PATH);
       break;
     default:
-      printf("%s: command not found\n", command.cmd);
+      if (path_lookup(PATH, command.cmd) == NULL)
+      {
+        printf("%s: command not found\n", command.cmd);
+      }
+      else
+      {
+        int rc = exec_prog(command.argv);
+        if (rc != 0)
+        {
+          perror("child program failed to run");
+        }
+      }
       break;
     }
   }
