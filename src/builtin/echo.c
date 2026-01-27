d90a7987d41c563dc8158b7d627d04454f02df14
Paul@WSL <paul.popescu2003@gmail.com>
2026-01-27 17:21:43 +0200

    codecrafters submit [skip ci]

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "shell.h"
 
 int exec_echo(const Command* command) {
     for (int i = 1; i < command->argc; i++) {
-        printf("%s ", command->argv[i]);
+        write(STDOUT_FILENO, command->argv[i], strlen(command->argv[i]));
+        if (i + 1 < command->argc) write(STDOUT_FILENO, " ", 1);
     }
-    printf("\n");
+
+    write(STDOUT_FILENO, "\n", 1);
+
     return 0;
 }
